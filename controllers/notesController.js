bac66cb7b532477f2822697705dd8d9cb9c5c4d4
Vignesh Kumar <reactivicky@gmail.com>
2023-10-10 12:09:54 +0530

    created controllers

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,6 +1,42 @@
-const getAllNotes = () => {};
+const Note = require('../models/noteModel');
 
-const createNote = () => {};
+const getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json({
+      status: 'success',
+      data: {
+        notes,
+      },
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'No notes found',
+    });
+  }
+};
+
+const createNote = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const newNote = await Note.create({
+      name,
+      description,
+    });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        note: newNote,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'failed',
+      message: 'Bad request',
+    });
+  }
+};
 
 module.exports = {
   getAllNotes,

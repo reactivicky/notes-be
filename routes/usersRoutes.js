c2f529053d0685a39107d58de4f80acc307511ad
Vignesh Kumar <reactivicky@gmail.com>
2023-10-18 17:51:11 +0530

    added get user by id

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { createUser, getAllUsers } = require('../controllers/usersController');
+const {
+  createUser,
+  getAllUsers,
+  getUser,
+} = require('../controllers/usersController');
 
 const router = express.Router();
 
 router.route('/signup').post(createUser);
 router.route('/').get(getAllUsers);
+router.route('/:id').get(getUser);
 
 module.exports = router;
